858f41665f07d6465bda3f8c91919549914267fa
robin trouillet <robintrouillet@hotmail.fr>
2021-11-11 16:13:17 +0100

    before css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # routes for lists
+  resources :lists, only: %i[index new create show] do
+    # routes for bookmarks
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: :destroy
 end
