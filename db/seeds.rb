2797b0d8f400ba7e06d63626f639bfbc2b7b66a2
robin trouillet <robintrouillet@hotmail.fr>
2021-11-12 16:00:40 +0100

    v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
 
 Movie.create(
   title: 'Wonder Woman 1984',
@@ -34,3 +36,12 @@ Movie.create(
   poster_url: 'https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg',
   rating: 7.0
 )
+
+
+url = 'http://tmdb.lewagon.com/movie/top_rated?&language=fr-FR'
+
+movies = JSON.parse(URI.open(url).read)
+movies["results"].each do |movie|
+  Movie.create(title: movie["title"], overview: movie["overview"], rating: movie["vote_average"],
+               poster_url: "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}")
+end
